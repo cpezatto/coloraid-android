bc35f767eb0303958f9fdbbaa8922d8c235934c9
cpezatto <cpezatto@gmail.com>
2025-06-27 10:28:55 +0200

    Creating Palette type selection

--- ColorAid/app/src/main/java/com/claudio/coloraid/data/loader/PaletteLoader.kt
+++ ColorAid/app/src/main/java/com/claudio/coloraid/data/loader/PaletteLoader.kt
@@ -4,22 +4,27 @@ import android.content.Context
 import android.util.Log
 import com.claudio.coloraid.R
 import com.claudio.coloraid.data.utils.ColorEntry
+import com.claudio.coloraid.palette.PaletteType
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import java.io.IOException
 import java.util.Locale
 
-fun loadPalette(context: Context, paletteName: String): List<ColorEntry> {
-    val locale = Locale.getDefault().language
-    val path = "palettes/${paletteName}_colors_${locale}.json"
+fun loadPalette(
+    context: Context,
+    paletteType: PaletteType,
+    locale: Locale = Locale.getDefault()
+): List<ColorEntry> {
+    val language = if (paletteType.hasTranslations) locale.language else "en"
+    val fileName = "palettes/${paletteType.filePrefix}_$language.json"
 
     return try {
         val inputStream = try {
-            context.assets.open(path)
+            context.assets.open(fileName)
         } catch (e: IOException) {
-            Log.e("PaletteLoader", context.getString(R.string.palette_file_not_found, path))
-            throw IllegalStateException(context.getString(R.string.palette_file_missing_exception, path))
+            Log.e("PaletteLoader", context.getString(R.string.palette_file_not_found, fileName))
+            throw IllegalStateException(context.getString(R.string.palette_file_missing_exception, fileName))
         }
 
         val mapper = ObjectMapper().registerKotlinModule()
--- ColorAid/app/src/main/java/com/claudio/coloraid/viewmodel/MainViewModelFactory.kt
+++ ColorAid/app/src/main/java/com/claudio/coloraid/viewmodel/MainViewModelFactory.kt
@@ -5,10 +5,15 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.claudio.coloraid.data.loader.loadPalette
 import com.claudio.coloraid.domain.usecase.DetectColorUseCase
+import com.claudio.coloraid.palette.PaletteType
 
-class MainViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
+
+class MainViewModelFactory(
+    private val context: Context,
+    private val paletteType: PaletteType
+) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        val palette = loadPalette(context, "standard")
+        val palette = loadPalette(context, paletteType)
         val useCase = DetectColorUseCase(palette)
         return MainViewModel(useCase) as T
     }
